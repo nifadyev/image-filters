11e01cd8db4e839a4b9b2270e0515e3ac96f5d4e
Neaph <vnifadyev@gmail.com>
2018-02-27 14:17:27 +0300

    Changed convertion to gray scale

--- Filters/SobolFilter.cs
+++ Filters/SobolFilter.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -7,21 +10,16 @@ using System.Threading.Tasks;
 
 namespace ImageFilters
 {
+    //commit: change converting gray scale
     class SobolFilter : MatrixFilter
     {
         private float[,] kernelY = { {-1, -2, -1 },
-                                       { 0, 0, 0   },
-                                       { 1, 2, 1}  };
+                                     { 0, 0, 0   },
+                                     { 1, 2, 1}  };
 
         private float[,] kernelX = { {-1, -2, 1 },
-                                       { -2, 0, 2 },
-                                       { -1, 2, 1}};
-
-        public SobolFilter()
-        {
-            //kernel = new float[3,3] { { -1, -2, 1 }, { -2, 0, 2 }, { -1, 2, 1 } };
-            //kernel = { { -1, -2, 1 },{ -2, 0, 2 },{ -1, 2, 1}};
-        }
+                                     { -2, 0, 2 },
+                                     { -1, 2, 1}};
 
         protected override Color CalculateNewPixelColor(Bitmap sourceImage, int x, int y)
         {
@@ -38,8 +36,6 @@ namespace ImageFilters
             float resultGkernelY = 0;
             float resultBkernelY = 0;
 
-            int intensity;
-
             for (int l = -radiusYkernelX; l <= radiusYkernelX; l++)
             {
                 for (int k = -radiusXkernelX; k <= radiusXkernelX; k++)
@@ -47,10 +43,6 @@ namespace ImageFilters
                     int idX = Clamp(x + k, 0, sourceImage.Width - 1);
                     int idY = Clamp(y + l, 0, sourceImage.Height - 1);
                     Color neighborColor = sourceImage.GetPixel(idX, idY);
-                    // Применение GrayScale для NeighborColor
-                    intensity = (int)(0.36 * neighborColor.R + 0.53 * neighborColor.G + 0.11 * neighborColor.B);
-                    neighborColor = Color.FromArgb(intensity, intensity, intensity);
-
 
                     resultRkernelX += neighborColor.R * kernelX[k + radiusXkernelX, l + radiusYkernelX];
                     resultGkernelX += neighborColor.G * kernelX[k + radiusXkernelX, l + radiusYkernelX];
@@ -62,9 +54,12 @@ namespace ImageFilters
                 }
             }
 
-            return Color.FromArgb(Clamp((int)Math.Sqrt(resultRkernelX * resultRkernelX + resultRkernelY * resultRkernelY), 0, 255),
-                      Clamp((int)Math.Sqrt(resultGkernelX * resultGkernelX + resultGkernelY * resultGkernelY), 0, 255),
-                      Clamp((int)Math.Sqrt(resultBkernelX * resultBkernelX + resultBkernelY * resultBkernelY), 0, 255));
+            float resultR = (float)Math.Sqrt(resultRkernelX * resultRkernelX + resultRkernelY * resultRkernelY);
+            float resultG = (float)Math.Sqrt(resultGkernelX * resultGkernelX + resultGkernelY * resultGkernelY);
+            float resultB = (float)Math.Sqrt(resultBkernelX * resultBkernelX + resultBkernelY * resultBkernelY);
+            int intensity = (int)(resultR + resultG + resultB) / 3;
+
+            return Color.FromArgb(Clamp((int)intensity, 0, 255), Clamp((int)intensity, 0, 255), Clamp((int)intensity, 0, 255));
         }
     }
 }
