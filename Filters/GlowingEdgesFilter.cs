b146d510938d4110e006fbd4e242b27bea4e759d
Neaph <vnifadyev@gmail.com>
2018-02-27 14:18:43 +0300

    Created Glowing edges аilter

---
+++ Filters/GlowingEdgesFilter.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ImageFilters
+{
+    class GlowingEdgesFilter : MatrixFilter
+    {
+        //protected Color[] kernel;
+        //int width;
+        //int height;
+        //protected override Color CalculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        //{
+        //    int radiusX = width / 2;
+        //    int radiusY = height / 2;
+        //    for (int l = -radiusY; l <= radiusY; ++l)
+        //        for (int k = -radiusX; k <= radiusX; ++k)
+        //        {
+        //            int idX = Clamp(x + k, 0, sourceImage.Width - 1);
+        //            int idY = Clamp(y + l, 0, sourceImage.Height - 1);
+        //            kernel[(l + radiusX) * height + k + radiusY] = sourceImage.GetPixel(idX, idY);
+        //        }
+        //    Array.Sort(kernel);
+        //    return kernel[kernel.GetLength(0) / 2 * height + kernel.GetLength(1) / 2];
+        //}
+        //public GlowingEdgesFilter()
+        //{
+        //    height = 5;
+        //    width = 5;
+        //    kernel = new Color[height * width];
+        //}
+
+
+
+        //commit: change converting gray scale
+        private float[,] kernelY = { {-1, -2, -1 },
+                                     { 0, 0, 0   },
+                                     { 1, 2, 1}  };
+
+        private float[,] kernelX = { {-1, -2, 1 },
+                                     { -2, 0, 2 },
+                                     { -1, 2, 1}};
+
+        protected override Color CalculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            int radiusXkernelX = kernelX.GetLength(0) / 2;
+            int radiusYkernelX = kernelX.GetLength(1) / 2;
+            int radiusXkernelY = kernelY.GetLength(0) / 2;
+            int radiusYkernelY = kernelY.GetLength(1) / 2;
+
+            float resultRkernelX = 0;
+            float resultGkernelX = 0;
+            float resultBkernelX = 0;
+
+            float resultRkernelY = 0;
+            float resultGkernelY = 0;
+            float resultBkernelY = 0;
+
+            for (int l = -radiusYkernelX; l <= radiusYkernelX; l++)
+            {
+                for (int k = -radiusXkernelX; k <= radiusXkernelX; k++)
+                {
+                    int idX = Clamp(x + k, 0, sourceImage.Width - 1);
+                    int idY = Clamp(y + l, 0, sourceImage.Height - 1);
+                    Color neighborColor = sourceImage.GetPixel(idX, idY);
+
+                    resultRkernelX += neighborColor.R * kernelX[k + radiusXkernelX, l + radiusYkernelX];
+                    resultGkernelX += neighborColor.G * kernelX[k + radiusXkernelX, l + radiusYkernelX];
+                    resultBkernelX += neighborColor.B * kernelX[k + radiusXkernelX, l + radiusYkernelX];
+
+                    resultRkernelY += neighborColor.R * kernelY[k + radiusXkernelY, l + radiusYkernelY];
+                    resultGkernelY += neighborColor.G * kernelY[k + radiusXkernelY, l + radiusYkernelY];
+                    resultBkernelY += neighborColor.B * kernelY[k + radiusXkernelY, l + radiusYkernelY];
+                }
+            }
+
+            float resultR = (float)Math.Sqrt(resultRkernelX * resultRkernelX + resultRkernelY * resultRkernelY);
+            float resultG = (float)Math.Sqrt(resultGkernelX * resultGkernelX + resultGkernelY * resultGkernelY);
+            float resultB = (float)Math.Sqrt(resultBkernelX * resultBkernelX + resultBkernelY * resultBkernelY);
+            //int intensity = (int)(resultR + resultG + resultB) / 3;
+
+            return Color.FromArgb(Clamp((int)resultR, 0, 255), Clamp((int)resultG, 0, 255), Clamp((int)resultB, 0, 255));
+        }
+    }
+}
